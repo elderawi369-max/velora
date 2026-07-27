237006256bbe74309a200ff6eb26f1add937395d
mohamed elderawi <elderawi369@gmail.com>
2026-07-27 13:48:30 +0300

    Add Android edge-to-edge release prep

--- frontend/android/app/src/main/java/com/velorachat/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/velorachat/app/MainActivity.java
@@ -1,8 +1,14 @@
 package com.velorachat.app;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.getcapacitor.BridgeActivity;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends BridgeActivity {
     @Override
@@ -10,5 +16,20 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(AppBadgePlugin.class);
         registerPlugin(GooglePlayBillingPlugin.class);
         super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        View contentView = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(contentView, (view, windowInsets) -> {
+            Insets systemBarsInsets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(
+                systemBarsInsets.left,
+                systemBarsInsets.top,
+                systemBarsInsets.right,
+                systemBarsInsets.bottom
+            );
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(contentView);
     }
 }
